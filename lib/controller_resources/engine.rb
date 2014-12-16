b302418961a2d65baf81f28cf8ef4c0a90e924bd
Tom Scott <tscott@telvue.com>
2014-12-16 18:43:49 -0500

    Safer way of including into app controller

--- lib/controller_resources/engine.rb
+++ lib/controller_resources/engine.rb
@@ -1,10 +1,9 @@
 module ControllerResources
   class Engine < ::Rails::Engine
     isolate_namespace ControllerResources
-    config.to_prepare do
-      require 'controller_resources'
 
-      class ApplicationController < ActionController::Base
+    config.to_prepare do
+      ApplicationController.class_eval do
         include ControllerResources::Extension
       end
     end
