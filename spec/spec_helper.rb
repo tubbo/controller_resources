edb5f0f23baf2be0afb7aba6e9edaaec677deedd
Tom Scott <tscott@weblinc.com>
2015-03-29 02:59:07 -0400

    Disable the dummy app init for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,8 @@ require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
 require "bundler/setup"
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
+#require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+#require "rails/test_help"
 require "rspec"
 
 Rails.backtrace_cleaner.remove_silencers!
@@ -15,9 +15,9 @@ Rails.backtrace_cleaner.remove_silencers!
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 # Load fixtures from the engine
-if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
-end
+#if ActiveSupport::TestCase.method_defined?(:fixture_path=)
+#  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+#end
 
 RSpec.configure do |config|
 end
